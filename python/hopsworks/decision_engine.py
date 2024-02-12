decffe8939449ee8a6fd68cddea560dc7830e9cf
Kateryna Herashchenko <kategera6@gmail.com>
2024-02-12 17:29:18 +0100

    build models added

--- python/hopsworks/decision_engine.py
+++ python/hopsworks/decision_engine.py
@@ -1,6 +1,14 @@
+import pickle
 from abc import ABC, abstractmethod
+from typing import List
+
 import humps
 import pandas
+import tensorflow as tf
+from tensorflow.keras.layers.experimental.preprocessing import StringLookup
+from dataclasses import dataclass
+from hsml.schema import Schema
+from hsml.model_schema import ModelSchema
 
 from hsfs import connection as hsfs_conn
 from hsml import connection as hsml_conn
@@ -10,6 +18,7 @@ class DecisionEngine(ABC):
     def __init__(self, configs_dict):
         self._name = configs_dict['name']
         self._configs_dict = configs_dict
+        self._catalog_df = None
 
         self._fs = hsfs_conn().get_feature_store(configs_dict['feature_store'])
         self._mr = hsml_conn().get_model_registry()
@@ -39,6 +48,10 @@ class DecisionEngine(ABC):
         pass
 
     @abstractmethod
+    def run_data_validation(self):
+        pass
+
+    @abstractmethod
     def build_models(self):
         pass
 
@@ -59,16 +72,129 @@ class RecommendationDecisionEngine(DecisionEngine):
             online_enabled=True,
         )
 
-        df = pandas.read_csv(catalog_config['file_path'])
-        fg.insert(df)
+        self._catalog_df = pandas.read_csv(catalog_config['file_path'])
+        # fg.insert(self._catalog_df)
+
+    def run_data_validation(self):
+        pass
 
     def build_models(self):
-        # Implement logic to create recommendation engine models based on config
-        pass
+
+        # Creating retrieval model
+        retrieval_config = self._configs_dict['model_configuration']['retrieval_model']
+
+        catalog_idx_config = CatalogIndexConfig(**retrieval_config['catalog_index_config'])
+        emb_dim = retrieval_config['item_space_dim']
+
+        item_id_list = self._catalog_df.iloc[:, catalog_idx_config.item_id_index].astype(str).unique().tolist()
+        name_list = self._catalog_df.iloc[:, catalog_idx_config.name_index].astype(str).unique().tolist()
+        category_list = self._catalog_df.iloc[:, catalog_idx_config.category_index].astype(str).unique().tolist()
+
+        item_model = ItemCatalogEmbedding(item_id_list, name_list, category_list, catalog_idx_config, emb_dim)
+
+        tf.saved_model.save(item_model, "embedding_model")
+
+        embedding_model_input_schema = Schema(self._catalog_df)
+        embedding_model_output_schema = Schema([{
+            "name": "embedding",
+            "type": "double",
+            "shape": [emb_dim],
+        }])
+
+        embedding_model_schema = ModelSchema(
+            input_schema=embedding_model_input_schema,
+            output_schema=embedding_model_output_schema
+        )
+        embedding_example = self._catalog_df.sample().to_dict("records")
+
+        mr_embedding_model = self._mr.tensorflow.create_model(
+            name="embedding_model",
+            description="Model that generates embeddings from items catalog features",
+            input_example=embedding_example,
+            model_schema=embedding_model_schema,
+        )
+        mr_embedding_model.save("embedding_model")
+
+        # Creating ranking model placeholder
+        file_name = 'ranking_model.pkl'
+        with open(file_name, 'wb') as file:
+            pickle.dump({}, file)
+
+        ranking_model = self._mr.python.create_model(name="ranking_model",
+                                               description="Ranking model that scores item candidates")
+        ranking_model.save(model_path='ranking_model.pkl')
 
     def build_deployments(self):
         # Implement logic to deploy recommendation engine models
         pass
+
+
+@dataclass
+class CatalogIndexConfig:
+    item_id_index: int = 0
+    name_index: int = 1
+    price_index: int = 2
+    category_index: int = 3
+
+
+class ItemCatalogEmbedding(tf.keras.Model):
+
+    def __init__(self, item_id_list: List[str], category_list: List[str], name_list: List[str],
+                 catalog_idx_config: CatalogIndexConfig, emb_dim: int):
+        super().__init__()
+
+        if emb_dim <= 0:
+            raise ValueError("emb_dim must be a positive integer")
+
+        if catalog_idx_config is None:
+            catalog_idx_config = CatalogIndexConfig()
+
+        self.item_embedding = tf.keras.Sequential([
+            StringLookup(
+                vocabulary=item_id_list,
+                mask_token=None
+            ),
+            tf.keras.layers.Embedding(
+                # We add an additional embedding to account for unknown tokens.
+                len(item_id_list) + 1,
+                emb_dim
+            )
+        ])
+
+        self.normalized_price = tf.keras.layers.Normalization(axis=None)
+        self.category_tokenizer = tf.keras.layers.StringLookup(
+            vocabulary=category_list, mask_token=None
+        )
+        self.category_list_len = len(category_list)
+        self.name_tokenizer = tf.keras.layers.StringLookup(
+            vocabulary=name_list, mask_token=None
+        )
+        self.fnn = tf.keras.Sequential([
+            tf.keras.layers.Dense(emb_dim, activation="relu"),
+            tf.keras.layers.Dense(emb_dim)
+        ])
+
+        self.catalog_idx_config = catalog_idx_config
+
+    def call(self, inputs):
+        item_id = inputs[:, self.catalog_idx_config.item_id_index]
+        category = inputs[:, self.catalog_idx_config.category_index]
+        price = inputs[:, self.catalog_idx_config.price_index]
+
+        category_embedding = tf.one_hot(
+            self.category_tokenizer(category),
+            self.category_list_len
+        )
+
+        concatenated_inputs = tf.concat([
+            self.item_embedding(item_id),
+            tf.reshape(self.normalized_price(price), (-1, 1)),
+            category_embedding,
+        ], axis=1)
+
+        outputs = self.fnn(concatenated_inputs)
+
+        return outputs
 
 
 class SearchDecisionEngine(DecisionEngine):
@@ -77,6 +203,9 @@ class SearchDecisionEngine(DecisionEngine):
 
     def build_catalog(self):
         # Implement logic to create feature groups for search engine based on config
+        pass
+
+    def run_data_validation(self):
         pass
 
     def build_models(self):
