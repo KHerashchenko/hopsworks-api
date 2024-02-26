55fe7453dac41048e2cec7cea4e172e4ad0742a8
Kateryna Herashchenko <kategera6@gmail.com>
2024-02-26 15:27:11 +0100

    fix

--- python/hopsworks/decision_engine.py
+++ python/hopsworks/decision_engine.py
@@ -134,7 +134,8 @@ class RecommendationDecisionEngine(DecisionEngine):
         fg.save(features=item_features)
 
         self._catalog_df = pd.read_csv(catalog_config['file_path'],
-                                       parse_dates=[feat for feat, val in catalog_config['schema'].items() if val['type'] == 'datetime'])
+                                       parse_dates=[feat for feat, val in catalog_config['schema'].items() if val['type'] == 'datetime'],
+                                       keep_default_na=False)
         fg.insert(self._catalog_df[catalog_config['schema'].keys()])
         # fv.add_tag(name="decision_engine", value={"use_case": self._configs_dict['use_case'], "name": self._configs_dict['name']})
 
